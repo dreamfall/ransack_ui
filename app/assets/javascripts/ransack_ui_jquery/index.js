7ee4b34243fe14309e0f4e8565f762f2c0c20145
Cristian Guerrero <cristian@s9.cl>
2017-02-01 14:15:29 -0800

    Add comment to explain compatibility hack

--- app/assets/javascripts/ransack_ui_jquery/index.js
+++ app/assets/javascripts/ransack_ui_jquery/index.js
@@ -1,6 +1,9 @@
 //= require ransack/predicates
 //= require ransack_ui_jquery/search_form
 
+// Select2 ver 3 checks if select2 is present by checking window.Select2
+// Version 4 uses jQuery.fn.select2 instead as a check
+// The following enables compatibility with select2 version 4
 if(jQuery.fn.select2) {
   window.Select2 = jQuery.fn.select2;
 }
